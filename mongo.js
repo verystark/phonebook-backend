91e1b8e21dca149d099efe216bab2150c99baf6d
verystark <tuomaspaloheimo05@gmail.com>
2026-07-10 12:10:34 +0300

    tietokanta komentoriviltä

--- mongo.js
+++ mongo.js
@@ -25,15 +25,26 @@ const personSchema = new mongoose.Schema({
 // määritellään kokoelma ja se millaisia dokumentteja tulee
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
-    id: '88',
-    name: 'Patrick Kane',
-    number: '0408804884',
-})
+if (process.argv.length === 5) {
 
-// Talleta henkilö tietokantaan
-person.save().then(result => {
-    console.log('person saved')
-    // Katkaise yhteys
-    mongoose.connection.close()
-})
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+
+    // Talleta henkilö tietokantaan
+    person.save().then(result => {
+        console.log('person saved')
+        // Katkaise yhteys
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 3) {
+    
+    Person.find({}).then(result => {
+        console.log('phonebook:')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
