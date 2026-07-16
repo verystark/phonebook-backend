c5700adfadcb653c350bdfedef59edfa70835062
verystark <tuomaspaloheimo05@gmail.com>
2026-07-16 12:50:00 +0300

    puhelinluettelo ja tietokanta step7, backend

--- index.js
+++ index.js
@@ -12,28 +12,6 @@ app.use(express.static('dist'))
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 
-let persons = [
-    {
-        id: "1",
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: "2",
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: "3",
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: "4",
-        name: "Mary Poppendieck",
-        number: "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(people => {
@@ -70,12 +48,8 @@ app.delete('/api/persons/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
-
-    if (!body.name || !body.number) {
-        return response.status(400).json({ error: "content missing" })
-    }
 
     Person.findOne({ name: body.name })
         .then(person => {
@@ -92,6 +66,7 @@ app.post('/api/persons', (request, response) => {
                 person.save().then(savedPerson => {
                     response.json(savedPerson)
                 })
+                .catch(error => next(error))
             }
         })
 })
@@ -124,6 +99,8 @@ const errorHandler = (error, request, response, next) => {
 
     if (error.name === 'CastError') {
         return response.status(400).send({ error: 'malformed id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
     }
 
     next(error)
